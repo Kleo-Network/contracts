a39f872b727e0e0566ac14bc2c8ecb6b4bd30050
Vaibhav Maheshwari <vaibhav.dkm@gmail.com>
2023-09-11 19:32:16 +0530

    Update lib.rs

--- solana/lib.rs
+++ solana/lib.rs
@@ -1,7 +1,7 @@
 use anchor_lang::prelude::*;
 use anchor_lang::solana_program::entrypoint::ProgramResult;
 
-declare_id!("7Ji1tL59iDNvPeAXthfqujSYKV98Gh5WnvkQ1aZDPbZ9");
+declare_id!("HhHj2W1hRFmQtW2cpR1PPJD88k2tcc9fMRHyV1jhYsCQ");
 
 #[program]
 pub mod kleo_sol {
@@ -11,9 +11,23 @@ pub mod kleo_sol {
     pub fn create_organisation(
         ctx: Context<CreateOrg>,
         name: String,
-        avatar: String,
+        intent: String,
+        website: String,
     ) -> ProgramResult {
-        
+        let org_account = &mut ctx.accounts.org_account;
+        let authority = &mut ctx.accounts.authority;
+
+        org_account.name = name;
+        org_account.intent = intent;
+        org_account.website = website;
+
+        org_account.authority = authority.key();
+
+        emit!(OrganisationEvent {
+            name: org_account.name.to_string(),
+            website: org_account.website.to_string(),
+            intent: org_account.intent.to_string()
+        });
 
         Ok(())
     }
@@ -23,7 +37,7 @@ pub mod kleo_sol {
 #[derive(Accounts)]
 pub struct CreateOrg<'info> {
     #[account(init, payer = authority, space = 8 + 2048 + 20 + 20 + 2048 + 4 + 8 + 8)]
-    pub user_account: Account<'info, Organisation>,
+    pub org_account: Account<'info, Organisation>,
     #[account(mut)]
     pub authority: Signer<'info>,
     pub system_program: Program<'info, System>,
@@ -31,10 +45,10 @@ pub struct CreateOrg<'info> {
 
 #[event]
 pub struct OrganisationEvent {
-    pub name: String,           // 20
-    pub intent: String,         // 2048
-    pub webite: String,         // 20
-    pub kleo: Vec<CookieBlock>, // 552? => 2048
+    pub name: String,   // 20
+    pub intent: String, // 2048
+    pub website: String, // 20
+                        // pub kleo: Vec<CookieBlock>, // 552? => 2048
 }
 
 #[account]
